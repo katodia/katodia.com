f13229a445059a65ce719ef5ab6505da45710a7b
sonirico <marsanben92@hotmail.com>
2016-10-17 16:42:27 +0200

    Implements form validation
    A lazy validation system. So lazy like the HTML5 default one.

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,33 +1,62 @@
+const validationFieldIds = [
+    'email', 'subject', 'message'
+];
+
 jQuery(document).ready(function () {
 
-    $button = jQuery('#submitter'); 
+    $button = jQuery('#submitter');
+    $form = jQuery('form[name="contact-form"]');
+
     $button.click(function () {
 
         resetAlerts();
-        $button.prop('disabled', true);
-        $contactForm = jQuery('form[name="contact-form"]');
+        $button.prop('invalid', false);
 
-        var $request = jQuery.ajax({
-            url: $contactForm.attr('action'),
-            method: $contactForm.attr('method'),
-            data: $contactForm.serialize(),
-            dataType: "json",
-            beforeSend: function() {
-			    jQuery('.alert-form.alert-info').css({'display': 'block'});
-		    },
-        });
+        // Validates form fields via HTML5 attributes
+        var formValid = true; 
+        for (i in validationFieldIds){
+            if (! document.getElementById(validationFieldIds[i]).checkValidity()) {
+                formValid = false;
+                break;
+            }    
+        }
 
-        $request.done(function (msj) {
-            resetAlerts();
-            $button.prop('disabled', false);
-            jQuery('.alert-form.alert-success').css({'display': 'block'});
-        });
+        if (formValid) {
+            
+            $button.prop('disabled', true);
+            $contactForm = jQuery('form[name="contact-form"]');
 
-        $request.fail(function (msj) {
-            resetAlerts();
-            $button.prop('disabled', false);
-            jQuery('.alert-form.alert-danger').css({'display': 'block'});
-        });
+            var $request = jQuery.ajax({
+                url: $contactForm.attr('action'),
+                method: $contactForm.attr('method'),
+                data: $contactForm.serialize(),
+                dataType: "json",
+                beforeSend: function() {
+                    jQuery('.alert-form.alert-info').css({'display': 'block'});
+                },
+            });
+
+            $request.done(function (msj) {
+                resetAlerts();
+                $button.prop('disabled', false);
+                jQuery('.alert-form.alert-success').css({'display': 'block'});
+
+                // Clear all fields
+                for (i in validationFieldIds){
+                    jQuery('#' + validationFieldIds[i]).val('').text('');   
+                }
+
+                $button.prop('invalid', false);
+            });
+
+            $request.fail(function (msj) {
+                resetAlerts();
+                $button.prop('disabled', false);
+                jQuery('.alert-form.alert-danger').css({'display': 'block'});
+            });
+        } else {
+            $form.find(':submit').click();
+        }
     });
 
     function resetAlerts () {
